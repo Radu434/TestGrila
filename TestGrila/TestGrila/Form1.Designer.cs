665dde9da11285cb547dfe4ae630de3a3fd965f9
Radu434 <47888258+Radu434@users.noreply.github.com>
2019-03-06 22:29:31 +0200

    Add files via upload

--- TestGrila/TestGrila/Form1.Designer.cs
+++ TestGrila/TestGrila/Form1.Designer.cs
@@ -29,9 +29,9 @@
         private void InitializeComponent()
         {
             this.gbRasp = new System.Windows.Forms.GroupBox();
-            this.rbRasp3 = new System.Windows.Forms.RadioButton();
-            this.rbRasp1 = new System.Windows.Forms.RadioButton();
-            this.rbRasp2 = new System.Windows.Forms.RadioButton();
+            this.C = new System.Windows.Forms.RadioButton();
+            this.A = new System.Windows.Forms.RadioButton();
+            this.B = new System.Windows.Forms.RadioButton();
             this.btnInainte = new System.Windows.Forms.Button();
             this.btnVerificare = new System.Windows.Forms.Button();
             this.btnTestNou = new System.Windows.Forms.Button();
@@ -56,53 +56,59 @@
             // 
             // gbRasp
             // 
-            this.gbRasp.Controls.Add(this.rbRasp3);
-            this.gbRasp.Controls.Add(this.rbRasp1);
-            this.gbRasp.Controls.Add(this.rbRasp2);
-            this.gbRasp.Location = new System.Drawing.Point(52, 96);
+            this.gbRasp.Controls.Add(this.C);
+            this.gbRasp.Controls.Add(this.A);
+            this.gbRasp.Controls.Add(this.B);
+            this.gbRasp.Location = new System.Drawing.Point(69, 118);
+            this.gbRasp.Margin = new System.Windows.Forms.Padding(4);
             this.gbRasp.Name = "gbRasp";
-            this.gbRasp.Size = new System.Drawing.Size(275, 60);
+            this.gbRasp.Padding = new System.Windows.Forms.Padding(4);
+            this.gbRasp.Size = new System.Drawing.Size(367, 74);
             this.gbRasp.TabIndex = 4;
             this.gbRasp.TabStop = false;
             // 
-            // rbRasp3
+            // C
             // 
-            this.rbRasp3.AutoSize = true;
-            this.rbRasp3.Location = new System.Drawing.Point(214, 19);
-            this.rbRasp3.Name = "rbRasp3";
-            this.rbRasp3.Size = new System.Drawing.Size(32, 17);
-            this.rbRasp3.TabIndex = 6;
-            this.rbRasp3.TabStop = true;
-            this.rbRasp3.Text = "C";
-            this.rbRasp3.UseVisualStyleBackColor = true;
+            this.C.AutoSize = true;
+            this.C.Location = new System.Drawing.Point(285, 23);
+            this.C.Margin = new System.Windows.Forms.Padding(4);
+            this.C.Name = "C";
+            this.C.Size = new System.Drawing.Size(38, 21);
+            this.C.TabIndex = 6;
+            this.C.TabStop = true;
+            this.C.Text = "C";
+            this.C.UseVisualStyleBackColor = true;
             // 
-            // rbRasp1
+            // A
             // 
-            this.rbRasp1.AutoSize = true;
-            this.rbRasp1.Location = new System.Drawing.Point(19, 19);
-            this.rbRasp1.Name = "rbRasp1";
-            this.rbRasp1.Size = new System.Drawing.Size(32, 17);
-            this.rbRasp1.TabIndex = 7;
-            this.rbRasp1.TabStop = true;
-            this.rbRasp1.Text = "A";
-            this.rbRasp1.UseVisualStyleBackColor = true;
+            this.A.AutoSize = true;
+            this.A.Location = new System.Drawing.Point(25, 23);
+            this.A.Margin = new System.Windows.Forms.Padding(4);
+            this.A.Name = "A";
+            this.A.Size = new System.Drawing.Size(38, 21);
+            this.A.TabIndex = 7;
+            this.A.TabStop = true;
+            this.A.Text = "A";
+            this.A.UseVisualStyleBackColor = true;
             // 
-            // rbRasp2
+            // B
             // 
-            this.rbRasp2.AutoSize = true;
-            this.rbRasp2.Location = new System.Drawing.Point(123, 19);
-            this.rbRasp2.Name = "rbRasp2";
-            this.rbRasp2.Size = new System.Drawing.Size(32, 17);
-            this.rbRasp2.TabIndex = 5;
-            this.rbRasp2.TabStop = true;
-            this.rbRasp2.Text = "B";
-            this.rbRasp2.UseVisualStyleBackColor = true;
+            this.B.AutoSize = true;
+            this.B.Location = new System.Drawing.Point(164, 23);
+            this.B.Margin = new System.Windows.Forms.Padding(4);
+            this.B.Name = "B";
+            this.B.Size = new System.Drawing.Size(38, 21);
+            this.B.TabIndex = 5;
+            this.B.TabStop = true;
+            this.B.Text = "B";
+            this.B.UseVisualStyleBackColor = true;
             // 
             // btnInainte
             // 
-            this.btnInainte.Location = new System.Drawing.Point(191, 187);
+            this.btnInainte.Location = new System.Drawing.Point(255, 230);
+            this.btnInainte.Margin = new System.Windows.Forms.Padding(4);
             this.btnInainte.Name = "btnInainte";
-            this.btnInainte.Size = new System.Drawing.Size(135, 47);
+            this.btnInainte.Size = new System.Drawing.Size(180, 58);
             this.btnInainte.TabIndex = 5;
             this.btnInainte.Text = "Inainte";
             this.btnInainte.UseVisualStyleBackColor = true;
@@ -110,18 +116,20 @@
             // 
             // btnVerificare
             // 
-            this.btnVerificare.Location = new System.Drawing.Point(191, 240);
+            this.btnVerificare.Location = new System.Drawing.Point(255, 295);
+            this.btnVerificare.Margin = new System.Windows.Forms.Padding(4);
             this.btnVerificare.Name = "btnVerificare";
-            this.btnVerificare.Size = new System.Drawing.Size(134, 47);
+            this.btnVerificare.Size = new System.Drawing.Size(179, 58);
             this.btnVerificare.TabIndex = 6;
             this.btnVerificare.Text = "Verificare";
             this.btnVerificare.UseVisualStyleBackColor = true;
             // 
             // btnTestNou
             // 
-            this.btnTestNou.Location = new System.Drawing.Point(51, 240);
+            this.btnTestNou.Location = new System.Drawing.Point(68, 295);
+            this.btnTestNou.Margin = new System.Windows.Forms.Padding(4);
             this.btnTestNou.Name = "btnTestNou";
-            this.btnTestNou.Size = new System.Drawing.Size(135, 47);
+            this.btnTestNou.Size = new System.Drawing.Size(180, 58);
             this.btnTestNou.TabIndex = 8;
             this.btnTestNou.Text = "Test Nou";
             this.btnTestNou.UseVisualStyleBackColor = true;
@@ -129,40 +137,46 @@
             // 
             // btnInapoi
             // 
-            this.btnInapoi.Location = new System.Drawing.Point(51, 187);
+            this.btnInapoi.Location = new System.Drawing.Point(68, 230);
+            this.btnInapoi.Margin = new System.Windows.Forms.Padding(4);
             this.btnInapoi.Name = "btnInapoi";
-            this.btnInapoi.Size = new System.Drawing.Size(134, 47);
+            this.btnInapoi.Size = new System.Drawing.Size(179, 58);
             this.btnInapoi.TabIndex = 9;
             this.btnInapoi.Text = "Inapoi";
             this.btnInapoi.UseVisualStyleBackColor = true;
+            this.btnInapoi.Click += new System.EventHandler(this.btnInapoi_Click);
             // 
             // lblIntrebare
             // 
             this.lblIntrebare.BackColor = System.Drawing.SystemColors.ControlLight;
-            this.lblIntrebare.Location = new System.Drawing.Point(52, 22);
+            this.lblIntrebare.Location = new System.Drawing.Point(69, 27);
+            this.lblIntrebare.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblIntrebare.Name = "lblIntrebare";
-            this.lblIntrebare.Size = new System.Drawing.Size(275, 54);
+            this.lblIntrebare.Size = new System.Drawing.Size(367, 66);
             this.lblIntrebare.TabIndex = 10;
             this.lblIntrebare.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblIntrebare.Click += new System.EventHandler(this.lblIntrebare_Click);
             // 
             // lblNr
             // 
             this.lblNr.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.lblNr.Location = new System.Drawing.Point(9, 33);
+            this.lblNr.Location = new System.Drawing.Point(12, 41);
+            this.lblNr.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblNr.Name = "lblNr";
-            this.lblNr.Size = new System.Drawing.Size(37, 32);
+            this.lblNr.Size = new System.Drawing.Size(49, 39);
             this.lblNr.TabIndex = 11;
-            this.lblNr.Text = "1";
             this.lblNr.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblNr.Click += new System.EventHandler(this.lblNr_Click);
             // 
             // cbLectie
             // 
             this.cbLectie.BackColor = System.Drawing.SystemColors.ButtonHighlight;
             this.cbLectie.ForeColor = System.Drawing.SystemColors.WindowText;
             this.cbLectie.FormattingEnabled = true;
-            this.cbLectie.Location = new System.Drawing.Point(362, 22);
+            this.cbLectie.Location = new System.Drawing.Point(483, 27);
+            this.cbLectie.Margin = new System.Windows.Forms.Padding(4);
             this.cbLectie.Name = "cbLectie";
-            this.cbLectie.Size = new System.Drawing.Size(200, 21);
+            this.cbLectie.Size = new System.Drawing.Size(265, 24);
             this.cbLectie.TabIndex = 12;
             // 
             // gbOptiuni
@@ -170,9 +184,11 @@
             this.gbOptiuni.Controls.Add(this.label2);
             this.gbOptiuni.Controls.Add(this.btnConfirma);
             this.gbOptiuni.Controls.Add(this.numConfirma);
-            this.gbOptiuni.Location = new System.Drawing.Point(362, 96);
+            this.gbOptiuni.Location = new System.Drawing.Point(483, 118);
+            this.gbOptiuni.Margin = new System.Windows.Forms.Padding(4);
             this.gbOptiuni.Name = "gbOptiuni";
-            this.gbOptiuni.Size = new System.Drawing.Size(200, 138);
+            this.gbOptiuni.Padding = new System.Windows.Forms.Padding(4);
+            this.gbOptiuni.Size = new System.Drawing.Size(267, 170);
             this.gbOptiuni.TabIndex = 13;
             this.gbOptiuni.TabStop = false;
             this.gbOptiuni.Text = "groupBox1";
@@ -181,24 +197,28 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label2.Location = new System.Drawing.Point(7, 47);
+            this.label2.Location = new System.Drawing.Point(9, 58);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(188, 13);
+            this.label2.Size = new System.Drawing.Size(241, 17);
             this.label2.TabIndex = 14;
             this.label2.Text = "Cate intrebari sa contina testul?";
             // 
             // btnConfirma
             // 
-            this.btnConfirma.Location = new System.Drawing.Point(137, 89);
+            this.btnConfirma.Location = new System.Drawing.Point(183, 110);
+            this.btnConfirma.Margin = new System.Windows.Forms.Padding(4);
             this.btnConfirma.Name = "btnConfirma";
-            this.btnConfirma.Size = new System.Drawing.Size(57, 22);
+            this.btnConfirma.Size = new System.Drawing.Size(76, 27);
             this.btnConfirma.TabIndex = 1;
             this.btnConfirma.Text = "Confirma";
             this.btnConfirma.UseVisualStyleBackColor = true;
+            this.btnConfirma.Click += new System.EventHandler(this.btnConfirma_Click);
             // 
             // numConfirma
             // 
-            this.numConfirma.Location = new System.Drawing.Point(11, 91);
+            this.numConfirma.Location = new System.Drawing.Point(15, 112);
+            this.numConfirma.Margin = new System.Windows.Forms.Padding(4);
             this.numConfirma.Maximum = new decimal(new int[] {
             10,
             0,
@@ -210,7 +230,7 @@
             0,
             0});
             this.numConfirma.Name = "numConfirma";
-            this.numConfirma.Size = new System.Drawing.Size(120, 20);
+            this.numConfirma.Size = new System.Drawing.Size(160, 22);
             this.numConfirma.TabIndex = 0;
             this.numConfirma.Value = new decimal(new int[] {
             3,
@@ -221,58 +241,78 @@
             // lstIntrebare
             // 
             this.lstIntrebare.FormattingEnabled = true;
-            this.lstIntrebare.Location = new System.Drawing.Point(52, 417);
+            this.lstIntrebare.ItemHeight = 16;
+            this.lstIntrebare.Items.AddRange(new object[] {
+            "asdf"});
+            this.lstIntrebare.Location = new System.Drawing.Point(69, 513);
+            this.lstIntrebare.Margin = new System.Windows.Forms.Padding(4);
             this.lstIntrebare.Name = "lstIntrebare";
-            this.lstIntrebare.Size = new System.Drawing.Size(95, 160);
+            this.lstIntrebare.Size = new System.Drawing.Size(125, 196);
             this.lstIntrebare.TabIndex = 14;
             this.lstIntrebare.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
             // 
             // lstRaspuns
             // 
             this.lstRaspuns.FormattingEnabled = true;
-            this.lstRaspuns.Location = new System.Drawing.Point(153, 417);
+            this.lstRaspuns.ItemHeight = 16;
+            this.lstRaspuns.Location = new System.Drawing.Point(204, 513);
+            this.lstRaspuns.Margin = new System.Windows.Forms.Padding(4);
             this.lstRaspuns.Name = "lstRaspuns";
-            this.lstRaspuns.Size = new System.Drawing.Size(95, 160);
+            this.lstRaspuns.Size = new System.Drawing.Size(125, 196);
             this.lstRaspuns.TabIndex = 15;
             this.lstRaspuns.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
             // 
             // lstA
             // 
             this.lstA.FormattingEnabled = true;
-            this.lstA.Location = new System.Drawing.Point(254, 417);
+            this.lstA.ItemHeight = 16;
+            this.lstA.Items.AddRange(new object[] {
+            "a"});
+            this.lstA.Location = new System.Drawing.Point(339, 513);
+            this.lstA.Margin = new System.Windows.Forms.Padding(4);
             this.lstA.Name = "lstA";
-            this.lstA.Size = new System.Drawing.Size(95, 160);
+            this.lstA.Size = new System.Drawing.Size(125, 196);
             this.lstA.TabIndex = 16;
             // 
             // lstB
             // 
             this.lstB.FormattingEnabled = true;
-            this.lstB.Location = new System.Drawing.Point(355, 417);
+            this.lstB.ItemHeight = 16;
+            this.lstB.Items.AddRange(new object[] {
+            "b"});
+            this.lstB.Location = new System.Drawing.Point(473, 513);
+            this.lstB.Margin = new System.Windows.Forms.Padding(4);
             this.lstB.Name = "lstB";
-            this.lstB.Size = new System.Drawing.Size(95, 160);
+            this.lstB.Size = new System.Drawing.Size(125, 196);
             this.lstB.TabIndex = 17;
             // 
             // lstC
             // 
             this.lstC.FormattingEnabled = true;
-            this.lstC.Location = new System.Drawing.Point(456, 417);
+            this.lstC.ItemHeight = 16;
+            this.lstC.Items.AddRange(new object[] {
+            "c"});
+            this.lstC.Location = new System.Drawing.Point(608, 513);
+            this.lstC.Margin = new System.Windows.Forms.Padding(4);
             this.lstC.Name = "lstC";
-            this.lstC.Size = new System.Drawing.Size(95, 160);
+            this.lstC.Size = new System.Drawing.Size(125, 196);
             this.lstC.TabIndex = 18;
             // 
             // lstCorect
             // 
             this.lstCorect.FormattingEnabled = true;
-            this.lstCorect.Location = new System.Drawing.Point(557, 417);
+            this.lstCorect.ItemHeight = 16;
+            this.lstCorect.Location = new System.Drawing.Point(743, 513);
+            this.lstCorect.Margin = new System.Windows.Forms.Padding(4);
             this.lstCorect.Name = "lstCorect";
-            this.lstCorect.Size = new System.Drawing.Size(95, 160);
+            this.lstCorect.Size = new System.Drawing.Size(125, 196);
             this.lstCorect.TabIndex = 19;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(711, 589);
+            this.ClientSize = new System.Drawing.Size(948, 725);
             this.Controls.Add(this.lstCorect);
             this.Controls.Add(this.lstC);
             this.Controls.Add(this.lstB);
@@ -288,6 +328,7 @@
             this.Controls.Add(this.btnVerificare);
             this.Controls.Add(this.btnInainte);
             this.Controls.Add(this.gbRasp);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.gbRasp.ResumeLayout(false);
@@ -301,9 +342,9 @@
 
         #endregion
         private System.Windows.Forms.GroupBox gbRasp;
-        private System.Windows.Forms.RadioButton rbRasp3;
-        private System.Windows.Forms.RadioButton rbRasp1;
-        private System.Windows.Forms.RadioButton rbRasp2;
+        private System.Windows.Forms.RadioButton C;
+        private System.Windows.Forms.RadioButton A;
+        private System.Windows.Forms.RadioButton B;
         private System.Windows.Forms.Button btnInainte;
         private System.Windows.Forms.Button btnVerificare;
         private System.Windows.Forms.Button btnTestNou;
--- TestGrila/TestGrila/Form1.cs
+++ TestGrila/TestGrila/Form1.cs
@@ -19,15 +19,26 @@ namespace TestGrila
         public Form1()
         {
             InitializeComponent();
+           
         }
         public int nrI = 0;
         public int corecte = 0;
-        int nr = 1;
+        public int nr = 1;
        
-
+        private void incarcaI(int i)
+        {
+            lblIntrebare.Text = lstIntrebare.Items[i].ToString();
+            A.Text = lstA.Items[i].ToString();
+            B.Text = lstB.Items[i].ToString();
+            C.Text = lstC.Items[i].ToString();
+            lblNr.Text = (nrI + 1).ToString();
+            A.Checked = false;
+            B.Checked = false;
+            C.Checked = false;
+        }
         private void btnTestNou_Click(object sender, EventArgs e)
         {
-            
+            incarcaI(nrI);
         }
 
         private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
@@ -42,7 +53,45 @@ namespace TestGrila
 
         private void btnInainte_Click(object sender, EventArgs e)
         {
+            nrI++;
+            if (nrI + 1 > lstIntrebare.Items.Count)
+            {
+                nrI--;
+                return;
+            }
+            else
+            {
+                incarcaI(nrI);
+            }
+        }
 
+        private void lblIntrebare_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnConfirma_Click(object sender, EventArgs e)
+        {
+            nr = Convert.ToInt32(numConfirma.Value);
+        }
+
+        private void btnInapoi_Click(object sender, EventArgs e)
+        {
+            nrI--;
+            if (nrI  < 0)
+            {
+                nrI = 0;
+                return;
+            }
+            else
+            {
+                incarcaI(nrI);
+            }
+        }
+
+        private void lblNr_Click(object sender, EventArgs e)
+        {
+            lblNr.Text = (nrI + 1).ToString();
         }
     }
 }
